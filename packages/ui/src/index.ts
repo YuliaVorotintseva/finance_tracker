b6e06f73726e354f5e47a68d059ddbba46f66dec
Yulia Vorotintseva <yulia.vorotintseva@gmail.com>
2026-06-23 15:52:01 +0600

    feat: add ui components with tailwind styles

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,9 @@
+export * from "./components/button";
+export * from "./components/input";
+export * from "./components/label";
+export * from "./components/card";
+export * from "./components/badge";
+export * from "./components/skeleton";
+export * from "./components/separator";
+export * from "./components/avatar";
+export { cn } from "./lib/utils";
